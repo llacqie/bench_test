472e8083da3b8094dba6a43ec964bc212753acaa
mekosko <135838138+mekosko@users.noreply.github.com>
2023-09-17 22:19:42 +0500

    Mehh

--- check.rs
+++ check.rs
@@ -2,39 +2,41 @@ use tokio::sync::mpsc;
 
 const SIZE: usize = 10000000usize;
 
+enum Step<T1> {
+    S1(T1),
+    S2(T1),
+}
+
 #[tokio::main]
 async fn main() {
-    let (ready, mut maybe_ready) = tokio::sync::mpsc::channel::<()>(1);
-
     let (event_sender, mut event_receiver) = mpsc::channel(SIZE);
 
-    let executor = event_sender.clone();
+    let channel = event_sender.clone();
 
     let inner = async move {
-        while let Some(maybe_maybe_event) = event_receiver.recv().await {
-            if let Some(maybe_event) = maybe_maybe_event {
-                if let Some(event) = maybe_event {
-                    if event == SIZE - 1 {
-                        ready.send(()).await.unwrap();
+        while let Some(event) = event_receiver.recv().await {
+            match event {
+                Step::S1(e) => {
+                    event_sender.send(Step::S2(e)).await.unwrap();
+                }
+                Step::S2(e) => {
+                    if e == SIZE - 1 {
+                        break;
                     };
-
-                    event_sender.send(Some(None)).await.unwrap();
-                } else {
-                    event_sender.send(None).await.unwrap();
                 }
             }
         }
     };
 
-    tokio::spawn(inner);
+    let jh = tokio::spawn(inner);
 
     let now = tokio::time::Instant::now();
 
     for e in 0..SIZE {
-        executor.send(Some(Some(e))).await.unwrap();
+        channel.send(Step::S1(e)).await.unwrap();
     }
 
-    maybe_ready.recv().await.unwrap();
+    jh.await.unwrap();
 
     println!("{}", now.elapsed().as_secs_f64());
 
